79316036fd3abf0e793e20d6383f596cb3c1c73f
DenisPepper <thech@yandex.ru>
2023-06-21 11:55:21 +0300

    добавил черновик открытки

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,20 @@
-import { AppRouter } from '../app-router/app-router';
 import './app.css';
+import React from "react";
+
+const BALLS = Array.from({length: 40});
 
 export const App = () => {
-  return (
-    <AppRouter/>
-  );
+
+    const getStyle = () => ({animationDuration: `${20 / Math.random()}s`} as React.CSSProperties);
+
+    const getClassName = () => Math.random() > 0.5 ? 'ball_option' : 'ball';
+
+    return (
+        <div className='app'>
+            <div className='balls'>
+                {BALLS.map((n, i) => <span key={i} className={getClassName()} style={getStyle()}></span>)}
+            </div>
+        </div>
+    )
+
 }
--- src/pages/main-page/main-page.tsx
+++
@@ -1,9 +0,0 @@
-import css from './main-page.module.css';
-
-export const MainPage = () => {
-    return(
-        <div className={css['main-page']}>
-            this is main page
-        </div>
-    );
-}
